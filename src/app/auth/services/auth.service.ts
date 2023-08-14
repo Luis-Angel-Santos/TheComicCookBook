5dda67ba2d94033013eebc3d96362f81f8d32e36
Luis Angel Santos <santosangel748@gmail.com>
2023-08-14 14:38:17 -0600

    Verificando si el jwt es valido

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject, signal, computed } from '@angular/core';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 
 @Injectable({
@@ -34,6 +34,34 @@ export class AuthService {
         }),
         map( () => true ),
         catchError( error => throwError( () => error.error.message ))
+      );
+
+  }
+
+  checkAuthStatus(): Observable<boolean>{
+
+    const url = `${ this.baseUrl }/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if(!token) return of(false);
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${ token }`);
+
+    return this.http.get<CheckTokenResponse>( url, { headers } )
+      .pipe(
+        map( ({ token, user }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem('token', token );
+          return true;
+        }),
+        catchError( () => {
+          this._currentUser.set( null );
+          this._authStatus.set( AuthStatus.notAuthenticated );
+          localStorage.removeItem('token');
+          return of(false);
+        } )
       );
 
   }
