c3c108a0fb112ac2dfec0c71aaa50f2e2db9a93d
Luis Angel Santos <santosangel748@gmail.com>
2023-08-16 11:07:27 -0600

    Css minify

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { HomeComponent } from './recipes/pages/home/home.component';
+import { RecetaComponent } from './recipes/pages/receta/receta.component';
 
 
 @NgModule({
   declarations: [
-    DashboardLayoutComponent
+    DashboardLayoutComponent,
+    HomeComponent,
+    RecetaComponent
   ],
   imports: [
     CommonModule,
