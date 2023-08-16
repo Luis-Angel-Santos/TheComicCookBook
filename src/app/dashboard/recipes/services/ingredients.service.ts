c2f6daa3204dc52d1159735fa89852ca7aba872b
Luis Angel Santos <santosangel748@gmail.com>
2023-08-16 10:48:30 -0600

    Servicio de ingredients y menus

--- src/app/dashboard/recipes/services/ingredients.service.ts
+++ src/app/dashboard/recipes/services/ingredients.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import { Ingredients, ComputeIngredientAmount, IngredientSubtitute } from '../interfaces/ingredients/ingredient.interface';
+import { IngredientInformation } from '../interfaces/ingredients/ingredientInformation.interface';
+import { IngredientToComplete } from '../interfaces/ingredients/ingredientToComplete.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientsService {
 
+  private urlAPI: string = `${environment.endPointAPI}food/ingredients/`;
+  private apiKey: string = environment.apiKey;
+  private httpClient: HttpClient = inject(HttpClient);
+
   constructor() { }
+
+  //buscar comidas por ingredientes
+  getIngredientsByName(name: string): Observable<Ingredients> {
+    return this.httpClient.get<Ingredients>(`${this.urlAPI}search?apiKey=${this.apiKey}&query=${name}&number=15`);
+  }
+
+  //obtener la informacion de un ingrediente
+  getIngredientInformation(idIngredient: number): Observable<IngredientInformation> {
+    return this.httpClient.get<IngredientInformation>(`${this.urlAPI}${idIngredient}/information?apiKey=${this.apiKey}`);
+  }
+
+  //obtener la cantidad de un ingrediente para lograr cierto objetivo nutricional
+  getComputeIngredientAmount(idIngredient: number, nutrient: string, target: number): Observable<ComputeIngredientAmount> {
+    return this.httpClient.get<ComputeIngredientAmount>(`${this.urlAPI}${idIngredient}?apiKey=${this.apiKey}&nutrient=${nutrient}&target=${target}`);
+  }
+
+  //autocompletar busqueda de ingredientes
+  getAutocompleteIngredients(query: string): Observable<IngredientToComplete[]> {
+    return this.httpClient.get<IngredientToComplete[]>(`${this.urlAPI}autocomplete?apiKey=${this.apiKey}&query=${query}&number=15`);
+  }
+
+  //obtener subtitutos de un ingrediente por su nombre
+  getSubstitutesByName(ingredientName: string): Observable<IngredientSubtitute> {
+    return this.httpClient.get<IngredientSubtitute>(`${this.urlAPI}substitutes?apiKey=${this.apiKey}&ingredientName=${ingredientName}`);
+  }
+
+  //obtener subtitutos de un ingrediente por su id
+  getSubstitutesById(idIngredient: number): Observable<IngredientSubtitute> {
+    return this.httpClient.get<IngredientSubtitute>(`${this.urlAPI}${idIngredient}/substitutes?apiKey=${this.apiKey}`);
+  }
+
 }
--- src/app/dashboard/recipes/services/menus.service.ts
+++ src/app/dashboard/recipes/services/menus.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import { MenuItems } from '../interfaces/menuItems/menuItems.interface';
+import { MenuItemDetail } from '../interfaces/menuItems/menuItemDetail.interface';
+import { MenuItemsSuggestions } from '../interfaces/menuItems/menuItemsSuggestions.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenusService {
 
+  private urlAPI: string = `${environment.endPointAPI}food/menuItems/`;
+  private apiKey: string = environment.apiKey;
+  private httpClient: HttpClient = inject(HttpClient);
+
   constructor() { }
+
+  //buscar algun item de un menu en todos los restaurantes
+  getMenuItem(menuItem: string): Observable<MenuItems> {
+    return this.httpClient.get<MenuItems>(`${this.urlAPI}search?query=${menuItem}&number=10&apiKey=${this.apiKey}`);
+  }
+
+  //obtener la informacion detallada de un item de un menu por id
+  getMenuItemDetailById(idMenuItem: string): Observable<MenuItemDetail> {
+    return this.httpClient.get<MenuItemDetail>(`${this.urlAPI}${idMenuItem}?apiKey=${this.apiKey}`);
+  }
+
+  //autocompletado de busqueda de items de menu
+  getMenuItemAutocomplete(menuItem: string): Observable<MenuItemsSuggestions> {
+    return this.httpClient.get<MenuItemsSuggestions>(`${this.urlAPI}suggest?query=${menuItem}&number=10&apiKey=${this.apiKey}`);
+  }
+
 }
